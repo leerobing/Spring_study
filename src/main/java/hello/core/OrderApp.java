1d2a3ec4a1ef91d4c8a1c33ad002d0b0fd0ca25a
leerobing <105533691+leerobing@users.noreply.github.com>
2023-06-22 16:37:59 +0900

    feat: 스프링으로 전환

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -4,21 +4,27 @@ import hello.core.member.*;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        OrderService orderService = appConfig.orderService();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        OrderService orderService = appConfig.orderService();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
 
         Member member = new Member(memberId,"어정잡이",Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId,"축구공",10000);
+        Order order = orderService.createOrder(memberId,"축구공",40000);
 
         System.out.println(order);
     }
