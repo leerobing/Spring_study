67e30203e9265b591b9a7a95153caf3b6f87cbfc
leerobing <105533691+leerobing@users.noreply.github.com>
2023-06-22 10:31:35 +0900

    feat: 새로운 할인 정책 개발

---
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,50 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("vip는 10% 할인이 적용되어야 한다.")
+    void vipDiscountTest() {
+        //given
+        Member member = new Member(1L, "어정잡이", Grade.VIP);
+
+        //when
+       int discountPrice = discountPolicy.discount(member,20000);
+
+        //then
+        assertThat(discountPrice).isEqualTo(2000);
+    }
+
+    @Test
+    @DisplayName("vip가 아니면 할인이 적용되지 않아야 한다.")
+    void basicDiscountTest() {
+
+        //given
+        Member member = new Member(2L, "어정잡삼", Grade.BASIC);
+
+
+        //when
+        int discountPrice = discountPolicy.discount(member,20000);
+
+        //then
+        assertThat(discountPrice).isEqualTo(0);
+
+
+    }
+}
