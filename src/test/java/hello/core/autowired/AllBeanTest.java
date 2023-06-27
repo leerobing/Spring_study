1695f3bf1818b8c051ee7da863797d0c74a52404
leerobing <105533691+leerobing@users.noreply.github.com>
2023-06-27 16:37:56 +0900

    feat: 조회한 빈이 모두 필요할때 List, Map 사용

---
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -0,0 +1,50 @@
+package hello.core.autowired;
+
+import hello.core.AppConfig;
+import hello.core.AutoAppConfig;
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class AllBeanTest {
+
+    @Test
+    void findAllBean() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class,DiscountService.class);
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L,"UserA", Grade.VIP);
+        int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+        assertThat(discountPrice).isEqualTo(1000);
+    }
+
+    static class DiscountService{
+        private final Map<String, DiscountPolicy> PolicyMap;
+        private final List<DiscountPolicy> PolicyList;
+
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policyList) {
+            PolicyMap = policyMap;
+            PolicyList = policyList;
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policyList = " + policyList);
+        }
+
+        public int discount(Member member, int price, String policy) {
+            DiscountPolicy discountPolicy = PolicyMap.get(policy);
+
+            return discountPolicy.discount(member,price);
+        }
+    }
+
+}
